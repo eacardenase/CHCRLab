f12f0856192657c58d2c99aa64331c54edc1a150
Edwin Cardenas <eacardenase@unal.edu.co>
2023-03-24 11:17:31 -0500

    feat: chcr on label and text field

--- CHCRLab/ViewController.swift
+++ CHCRLab/ViewController.swift
@@ -11,9 +11,55 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
     }
-
-
 }
 
+extension ViewController {
+    func setupViews() {
+        let nameLabel = makeLabel(withText: "Name")
+        let textField = makeTextField(withPlaceholder: "Enter name here")
+        
+        view.addSubview(nameLabel)
+        view.addSubview(textField)
+        
+        // nameLabel
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8)
+        ])
+        
+        // textLabel
+        NSLayoutConstraint.activate([
+            textField.firstBaselineAnchor.constraint(equalTo: nameLabel.firstBaselineAnchor),
+            textField.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 8),
+            textField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8)
+        ])
+        
+        // CHCR
+        nameLabel.setContentHuggingPriority(UILayoutPriority.defaultHigh, for: .horizontal)
+//        nameLabel.setContentHuggingPriority(UILayoutPriority(rawValue: 251), for: .horizontal)
+//        textField.setContentHuggingPriority(UILayoutPriority(rawValue: 249), for: .horizontal)
+    }
+    
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.backgroundColor = .yellow
+        
+        return label
+    }
+    
+    func makeTextField(withPlaceholder text: String) -> UITextField {
+        let textField = UITextField()
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = text
+        textField.backgroundColor = .lightGray
+        
+        return textField
+    }
+}
